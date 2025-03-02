e155b43f9b71e4069e5b2795a429be15a1bd0d0a
DarthNik <gruzdik05@bk.ru>
2025-03-02 16:33:08 +0300

    Update error.c

--- src/error.c
+++ src/error.c
@@ -5,7 +5,7 @@
 void errors(const char* lfp){
     FILE* fp;
     if (access(lfp, 0) == -1)
-        fp = fopen("error_file", "w");
+        fp = fopen("error_file.txt", "w");
     else {fp = fopen(lfp, "w");}
     if (access(lfp, 2) == -1){
 	fprintf(stderr, "can't open error file for writing"); 
